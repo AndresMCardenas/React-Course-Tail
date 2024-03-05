31d0dbe09e0f40c7f41f4189264d87e488703227
Andres M. Cardenas <andrescar18@gmail.com>
2024-03-05 14:06:31 -0500

    se crea conexion con la api

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,20 +1,20 @@
-const Card = () => {
+const Card = (data) => {
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 '>Electronics</span>
-        <img className='size-full object-cover rounded-lg ' src='https://images.pexels.com/photos/815494/pexels-photo-815494.jpeg' alt='headphones' ></img>
+        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 '></span>
+        <img className='size-full object-cover rounded-lg ' src={data.images} alt={data.title} ></img>
         <div className='absolute top-0 right-0 flex justify-center items-center bg-white size-6 rounded-full m-2 p-1'>
           +
         </div>
       </figure>
       <p className='flex justify-between'>
-        <span className='text-sm font-light'>Headphone</span>
-        <span className='text-lg font-medium'>$300</span>
+        <span className='text-sm font-light'>{data.title}</span>
+        <span className='text-lg font-medium'>${data.price}</span>
       </p>
     </div>
   )
-  
+    
 }
 
 export default Card;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from '../../Components/Card';
 import Layout from '../../Components/Layout';
 
 export default function Home() {
-  const [items, setItems] = useState([
-
-  ]);
+  const [items, setItems] = useState(null);
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  
+  }, [])
   return (
     <Layout>
       Home
+      {
+        items && items?.map(item => (
+          <Card key={item.id} {...item} />          
+        ))
+      }
       <Card />
     </Layout>    
   )
